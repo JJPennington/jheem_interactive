3e759d66d21730bc5b1c6615e3543053fafdfd72
joeflack4 <joeflack4@gmail.com>
2021-02-11 15:30:16 -0500

    Custom interventions updates - secondary panel bolding - secondary panel choices are now named vectors - demographic 'select all' toggles - num subpops: sliders --> dropdown - subpop tabset

--- visualization/shiny/ui/custom_interventions.R
+++ visualization/shiny/ui/custom_interventions.R
@@ -24,16 +24,18 @@ conditionalDropdown <- function(
       'input.', inputId.prefix, '_switch', i, ' == true'
       # '(input.selected_subpop == ', as.character(i), ')'
       ),
-      selectInput(
-        inputId=paste0(inputId.prefix, '_value', i),
-        label=selectInput.label,
-        choices=choices
-        # selected=choices[1]
+     selectInput.label,
+     selectInput(
+       inputId=paste0(inputId.prefix, '_value', i),
+       label='',
+       # label=selectInput.label,
+       choices=choices
+       # selected=choices[1]
     )))  # </selectInput/conditionalPanel/fluidRow>
   )  # /div>
 }
 
-customInterventionBox <- function(i) {
+subpopBox <- function(i) {
  # collapse = TRUE
  # customIntervention_box_switch.value = FALSE
  # if (i == 1) {
@@ -55,20 +57,26 @@ customInterventionBox <- function(i) {
    #  right=F,
    #  status='primary'),
    
-   conditionalPanel(
-     condition=paste0(
-       # '(input.customIntervention_box_switch', i, ' == true)'),
-       '(input.selected_subpop == ', i, ')'
-     ),
-    box(
-      title=paste0("Subpopulation ", i),
-      # collapsible=TRUE,
-      # collapsed=collapse,
-      status="primary",
-      width=NULL,
-      solidHeader=TRUE,
+   # conditionalPanel(
+   #   condition=paste0(
+   #     # '(input.customIntervention_box_switch', i, ' == true)'),
+   #     '(input.selected_subpop == ', i, ')'
+   #   ),
+    # box(
+    #   title=paste0("Subpopulation ", i),
+    #   # collapsible=TRUE,
+    #   # collapsed=collapse,
+    #   status="primary",
+    #   width=NULL,
+    #   solidHeader=TRUE,
+     wellPanel(
+       HTML('<h4>Subpop ', i,' demographics</h4>'),
        lapply(dimension.value.options,
            function(dim) {
+             switchId = paste0(dim[['name']], '_switch', i)
+             label.plural = paste0(tolower(dim[['label']]), 's')
+             if (dim[['name']] == 'sex')
+               label.plural = 'sexes'
              
              fluidRow(
                fluidRow(
@@ -76,23 +84,28 @@ customInterventionBox <- function(i) {
                ),  # </fluidRow>
                fluidRow(
                 materialSwitch(
-                 inputId=paste0(
-                  dim[['name']], '_switch', i),
-                 label='Select all',
+                 inputId=switchId,
+                 # label='Select all',
+                 label=paste0('Include all ', label.plural),
                  value=FALSE,
                  right=TRUE,
                  status='primary'),
-                checkboxGroupInput(
-                 inputId=paste0(dim[['name']], i),
-                 label=NULL,
-                 choiceNames=unname(dim[['choices']]),
-                 choiceValues=names(dim[['choices']]) )
+                # Got 'undefined' error for input val, prolly cuz hadnt loaded
+                # conditionalPanel(
+                  # condition=paste0('input.', switchId, ' == false'),
+                  checkboxGroupInput(
+                    inputId=paste0(dim[['name']], i),
+                    label=NULL,
+                    choiceNames=unname(dim[['choices']]),
+                    choiceValues=names(dim[['choices']]) )                  
+                # )
                ),  # </fluidRow>
              verticalSpacer(10)
              
        )})  # </fluidRow/lapply>
-   ))  # </conditionalPanel/Box>
-  )  # </fluidRow>
+   # ))  # </conditionalPanel/Box>
+   )  # </wellPanel>
+ )  # </fluidRow>
 }
 
 # UI ####
@@ -122,24 +135,35 @@ CUSTOM.CONTENT = tags$table(
     width=NULL, 
     size=NULL),
    
-   sliderInput(
-     inputId='n_subpops',
-     label='Number of subpopulations',
-     min=1,
-     max=5,
-     value=1,
-     step=1),
-   
    selectInput(
-     inputId='selected_subpop',
-     label='Editing subpopulation:',
-     # choices=c(1:input$n_subpops)),
-     # TODO
+     inputId='n_subpops',
+     label='Number of subpopulations to include',
      choices=c(1:5)),
    
-   map(
-     1:5,
-     function(i) customInterventionBox(i) )
+   HTML('<strong>Edit subpopulations:</strong>'),
+   tabsetPanel(
+     id='subpop_tabset_panel',
+     type='pills',
+     tabPanel(
+       title=1,
+       subpopBox(1)),
+     tabPanel(
+       title=2,
+       subpopBox(2)),
+     tabPanel(
+       title=3,
+       subpopBox(3)),
+     tabPanel(
+       title=4,
+       subpopBox(4)),
+     tabPanel(
+       title=5,
+       subpopBox(5))
+   ),
+   
+   # map(
+   #   1:5,
+   #   function(i) subpopBox(i) )
    
    )  # </div>
   ),  # </leftPanel>
@@ -154,47 +178,64 @@ CUSTOM.CONTENT = tags$table(
           conditionalPanel(
             condition=paste0(
               # '(input.customIntervention_box_switch', i, ' == true)'
-              '(input.selected_subpop == ', i, ')'
+              '(input.subpop_tabset_panel ==', i, ')'
             ),
-            sliderInput(
-              inputId=paste0('custom_interventions_years', i),
-              label='Years',
-              value=c(2020, 2030),
-              min=2020,
-              max=2030,
-              step=1),
-            conditionalDropdown(
-              i=i,
-              inputId.prefix='test_freq_months',
-              # box.title=div(icon('vial'), "HIV Testing"),
-              materialSwitch.label=HTML(
-                '<b>Include testing in intervention</b>'),
-              selectInput.label='Frequency of testing
-      (individuals in targeted subgroups are tested,
-      on average, once every so many months)',
-              choices=c(3, 6, 12, 24)),
-            conditionalDropdown(
-              i=i,
-              inputId.prefix='prep',
-              # box.title=div(icon('pills'), 'Pre-Exposure Prophylaxis (PrEP)'),
-              #alt icon: prescription-bottle-alt
-              materialSwitch.label=HTML(
-                '<b>Include PrEP in Intervention</b>'),
-              selectInput.label='Proportion on PrEP: (this percentage of
-      individuals in targeted subgroups are prescribed and
-      adherent to PrEP with HIV screening every 3mo)',
-              choices=c(10, 25, 15, 75)),
-            conditionalDropdown(
-              i=i,
-              inputId.prefix='viral_suppression',
-              # box.title=div(icon('virus'), 'Viral Suppression'),
-              materialSwitch.label=HTML(
-                '<b>Include viral suppression in Intervention</b>'),
-              selectInput.label='Suppressed Proportion: (this percentage of
-      PWH with diagnosed HIV in the targeted subgroups are virally
-      suppressed)',
-              choices=c(80, 85, 90, 95))
-      )}),  # </conditionalPanel/map>
+            wellPanel(
+              HTML('<h4>Subpop ', i,' intervention details</h4>'),
+              sliderInput(
+                inputId=paste0('custom_interventions_years', i),
+                label='Years',
+                value=c(2020, 2030),
+                min=2020,
+                max=2030,
+                step=1),
+              conditionalDropdown(
+                i=i,
+                inputId.prefix='test_freq_months',
+                # box.title=div(icon('vial'), "HIV Testing"),
+                materialSwitch.label=HTML(
+                  # '<b>Include testing in intervention</b>'),
+                  '<b>Include testing</b>'),
+                selectInput.label='Frequency of testing
+        (individuals in targeted subgroups are tested,
+        on average, once every so many months)',
+                choices=c(
+                  '3 months'=3, 
+                  '6 months'=6, 
+                  '12 months'=12, 
+                  '24 months'=24)),
+              conditionalDropdown(
+                i=i,
+                inputId.prefix='prep',
+                # box.title=div(icon('pills'), 'Pre-Exposure Prophylaxis (PrEP)'),
+                #alt icon: prescription-bottle-alt
+                materialSwitch.label=HTML(
+                  # '<b>Include PrEP in Intervention</b>'),
+                  '<b>Include PrEP</b>'),
+                selectInput.label='Proportion on PrEP: (this percentage of
+        individuals in targeted subgroups are prescribed and
+        adherent to PrEP with HIV screening every 3mo)',
+                choices=c(
+                  '10%'=0.1, 
+                  '25%'=0.25, 
+                  '15%'=0.15, 
+                  '75%'=0.75)),
+              conditionalDropdown(
+                i=i,
+                inputId.prefix='viral_suppression',
+                # box.title=div(icon('virus'), 'Viral Suppression'),
+                materialSwitch.label=HTML(
+                  # '<b>Include viral suppression in Intervention</b>'),
+                  '<b>Include viral suppression</b>'),
+                selectInput.label='Suppressed Proportion: (this percentage of
+        PWH with diagnosed HIV in the targeted subgroups are virally
+        suppressed)',
+                choices=c(
+                  '80%'=0.8, 
+                  '85%'=0.85, 
+                  '90%'=0.9, 
+                  '95%'=0.95))
+    ))}),  # </wellPanel/conditionalPanel/map>
   ),
   #-- The Main Panel --####
   tags$td(class='display_td',
