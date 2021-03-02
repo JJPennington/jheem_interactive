f6c5a8b9dceef53236a8651ab4440dfc5b8c3e86
joeflack4 <joeflack4@gmail.com>
2021-03-01 19:33:50 -0500

    Updated analytics.R to write to DB, following changes creation of this table in PostgreSQL

--- visualization/shiny/helpers/analytics.R
+++ visualization/shiny/helpers/analytics.R
@@ -1,5 +1,4 @@
-
-
+# Functions ####
 # This should have innate error handling and fail silently
 # The database needs to track all these, plus the date/time
 do.track.request <- function(session.id, #non-unique string
@@ -31,9 +30,69 @@ do.track.request <- function(session.id, #non-unique string
 )
 {
     tryCatch({
-        #put to the database
+        df = data.frame(
+            created_at=Sys.time(),
+            session_id=session.id,
+            called_from=called.from,
+            version=version,
+            location=location,
+            intervention_code=intervention.code,
+            n_target_subgroups=n.target.subgroups,
+            intervention_start=intervention.start,
+            intervention_end=intervention.end,
+            target_ages=target.ages,
+            target_races=target.races,
+            target_sexes=target.sexes,
+            target_risks=target.risks,
+            testing=testing,
+            prep=prep,
+            viral_suppression=viral.suppression,
+            outcomes=outcomes,
+            facet_by=facet.by,
+            split_by=split.by,
+            change_outcome_start=change.outcome.start,
+            change_outcome_end=change.outcome.end,
+            show_change=show.change,
+            plot_format=plot.format,
+            show_ages=show.ages,
+            show_races=show.races,
+            show_sexes=show.sexes,
+            show_risks=show.risks)
+        db.write.rows(
+            table.name='analytics',
+            data.df=df)
     },
     error=function(e){
-        log.error("there was an error")
+        log.error(e)
     })
 }
+
+# Testing ####
+do.track.request(
+    session.id='xxx', #non-unique string
+    called.from='xxx', #string
+    version='xxx', #string
+    location='xxx', #string
+    intervention.code='xxx', #string
+    n.target.subgroups=3, #integer
+    intervention.start=1.0, #numeric
+    intervention.end=1.0, #numeric
+    target.ages='xxx', #string
+    target.races='xxx', #string
+    target.sexes='xxx', #string
+    target.risks='xxx', #string
+    testing='', #may be NA
+    prep=NA, #may be NA
+    viral.suppression=NA, #may be NA
+    outcomes='xxx', #string
+    facet.by='xxx', #string
+    split.by='xxx', #string
+    change.outcome.start=1.5, #numeric
+    change.outcome.end=999.8492, #numeric
+    show.change=1.0, #numeric
+    plot.format='xxx', #string
+    show.ages='xxx', #string
+    show.races='xxx', #string
+    show.sexes='xxx', #string
+    show.risks='xxx' #string
+)
